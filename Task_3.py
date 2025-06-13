23df80a659734f1a06557bf911abba2f84ca84df
Mariia Timofieieva <30m2001m@gmail.com>
2025-06-13 15:01:58 +0300

    Add Task 3

---
+++ Task_3.py
@@ -0,0 +1,41 @@
+import re
+
+def normalize_phone(phone_number: str) -> str:
+    if not isinstance(phone_number, str):
+        return "Error! Phone number must be a string"
+    
+    digits = re.sub(r'[^\d+]', '', phone_number)
+
+    if len(phone_number) < 10:
+        return "Error! Phone number must be at least 10 characters long"
+    
+    if digits.startswith('+'):
+        return digits
+    else:
+        if digits.startswith('380'):
+            return '+' + digits
+        else:
+            return '+38' + digits
+        
+# Test cases
+# phones = [
+#     "    +38(050)123-32-34",
+#     "     0503451234",
+#     "(050)8889900",
+#     "38050-111-22-22",
+#     "38050 111 22 11   ",
+#     True,
+#     38078907543,
+#      "067\\t123 4567",
+#     "(095) 234-5678\\n",
+#     "+380 44 123 4567",
+#     "380501234567",
+#     "    +38(050)123-32-34",
+#     "     0503451234",
+#     "(050)8889900",
+#     "38050-111-22-22",
+#     "38050 111 22 11   ",
+# ]
+
+# for p in phones:
+#     print(normalize_phone(p))
